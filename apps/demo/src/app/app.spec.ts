0876b29ebed090ef16f36dfb9c74b4d17fc70cb3
Garkalne, Nora <nora.garkalne@visma.com>
2025-08-04 16:31:14 +0300

    tests fix

--- apps/demo/src/app/app.spec.ts
+++ apps/demo/src/app/app.spec.ts
@@ -1,11 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
-import { NxWelcome } from './nx-welcome';
+import { LoginPage } from '../app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App, NxWelcome],
+      imports: [App, LoginPage],
     }).compileComponents();
   });
 
@@ -13,6 +13,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome demo');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome Back');
   });
 });
